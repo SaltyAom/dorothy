01eaa663b2834fae183a2bad7e865a9fd6f3e321
saltyaom <saltyaom@gmail.com>
2023-12-30 05:01:16 +0700

    :broom: fix: resonator api

--- apps/medium/src/app/layout.tsx
+++ apps/medium/src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
                 <title>PRTS</title>
                 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📘</text></svg>" />
 
-                <link rel="manifest" href="assets/app/manifest.json" />
+                <link rel="manifest" href="/app/manifest.json" />
                 <meta name="mobile-web-app-capable" content="yes" />
 
                 <meta name="theme-color" content="#fbfff4" />
